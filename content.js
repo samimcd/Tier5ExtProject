8e28b7832e3713c1fa12df8bab45d31be39a86b0
samim akhtar <samimcse2@gmail.com>
2021-06-12 01:14:38 +0530

    Modifide by samim

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 console.log("Facebook Extension");
 
- var port = chrome.runtime.connect ({
+ var fbport = chrome.runtime.connect ({
     name: "ContentScript"
 }) 
 
@@ -19,16 +19,20 @@ var l = 0;
 
 const Startlike = (am ,c=0) => {
     var F = document.querySelector ('div[aria-label="Actions for this post"]');
+    console.log("Find the three dots");
     F.className = "j83agx80 cbu4d94t"; 
     if (F.className == "j83agx80 cbu4d94t") { 
-         console.log("Post Liked");
+         console.log("Reached to the like options");
          const T = am.time;
          var tInterval=setTimeout(() => {
          var lbtn = document.querySelector ('div[aria-label="Like"]');
          lbtn.click()
+         console.log("Post Liked");
          l++;
+         console.log("Find the next post");
+
          notifyUser(am,l);
-         Startlike(am,c+1)
+         Startlike(am,c++)
          }, T*1000 ); 
     }
     
@@ -36,6 +40,7 @@ const Startlike = (am ,c=0) => {
         return false;
     }
 } 
+
 
 
 
@@ -51,6 +56,6 @@ const notifyUser = (auto,c = 0) => {
           text: sentCount.toString(),
         }
       };
-    
-    port.postMessage(auto);
+  
+    fbport.postMessage(auto);
   };
