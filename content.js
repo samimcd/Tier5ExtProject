378b7a333d555eb4bc9e4ef628a7c021f2527d64
samim akhtar <samimcse2@gmail.com>
2021-06-17 01:55:54 +0530

    Updated file

--- content.js
+++ content.js
@@ -1,42 +1,37 @@
 console.log("Facebook Extension");
 
- var fbport = chrome.runtime.connect ({
+    var fbport = chrome.runtime.connect ({
     name: "ContentScript"
+}) 
+  chrome.runtime.onMessage.addListener (function(am){
+    console.log("data", am);
+      switch(am.action) {
+       case "Autoliker" : Startlike(am.fbook)
+      break;
+    }
+  return true;
 }) 
 
 
-chrome.runtime.onMessage.addListener (function(am){
-    console.log("data", am);
-    switch(am.action) {
-        case "Autoliker" : Startlike(am.fbook)
-        break;
-    }
-    return true;
-} ) 
-
-
 var l = 0;
-
 const Startlike = (am ,c=0) => {
     var F = document.querySelector ('div[aria-label="Actions for this post"]');
-    console.log("Find the three dots");
-    F.className = "j83agx80 cbu4d94t"; 
-    if (F.className == "j83agx80 cbu4d94t") { 
-         console.log("Reached to the like options");
-         const T = am.time;
-         var tInterval=setTimeout(() => {
-         var lbtn = document.querySelector ('div[aria-label="Like"]');
-         scrollToPos(lbtn)
-         lbtn.click()
-         
-         console.log("Post Liked");
-         l++;
-         console.log("Find the next post");
-
-         notifyUser(am,l);
-         Startlike(am,c++)
-         }, T*1000 ); 
-    }
+        console.log("Find the three dots");
+          F.className = "j83agx80 cbu4d94t"; 
+           if (F.className == "j83agx80 cbu4d94t") { 
+             console.log("Reached to the like options");
+               const T = am.time;
+                 var tInterval=setTimeout(() => {
+                   var lbtn = document.querySelector ('div[aria-label="Like"]');
+                  lbtn.click()
+                scrollToPos(lbtn)
+             console.log("Post Liked");
+          l++;
+        console.log("Find the next post");
+      notifyUser(am,l);
+    Startlike(am,c++)
+  }, T*1000 ); 
+}
     
     else {
         return false;
@@ -62,11 +57,11 @@ const notifyUser = (auto,c = 0) => {
     fbport.postMessage(auto);
   };
 
-  const scrollToPos = (el) => {
-    $("html,body").animate(
-     { 
-     scrollTop: $(el).offset().top - 15,
-     },
-     "slow"
-    );
+ const scrollToPos = (el) => {
+     $("html,body").animate(
+        { 
+        scrollTop: $(el).offset().top - 15,
+        },
+      "slow"
+     );
   };
